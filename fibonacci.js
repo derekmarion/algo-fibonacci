c77bc5c52dc4ccda8702853353b53811a514ec51
Derek Marion <derek.j.marion@gmail.com>
2023-10-04 07:49:10 -0700

    Initial commit for working verson of fibonacci.js that pasts all tests

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
-  return 0;
+  let fib = [0, 1]; //seed first two numbers in fibonacci sequence
+  if (num <= 1) { //handle cases where n is 0 or 1
+    return fib[num];
+  };
+  for (i = 2; i <= num; i++) { //build fib sequence up to place n
+    let current_value = fib[i-2] + fib[i - 1];
+    fib.push(current_value);
+  };
+  return fib.slice(-1)[0]; //return last value of fib sequence
 }
 
 module.exports = fibonacci;
